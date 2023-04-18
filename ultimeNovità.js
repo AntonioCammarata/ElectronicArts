fc618a26867c0e18e3cb234006190c05dacc721b
Stefano <maclion@live.com>
2023-04-19 00:43:16 +0200

    completato js menù buttons

--- ultimeNovità.js
+++ ultimeNovità.js
@@ -1,23 +1,24 @@
-const $trigger = document.querySelectorAll(".trigger");
-const $content = document.querySelectorAll(".content");
+// const $trigger = document.querySelectorAll(".trigger");
+// const $content = document.querySelectorAll(".content");
 
-$trigger.forEach($el => {
-  $el.addEventListener("click", (event) => {
-    // Prima levo la classe visible da tutti i `div.content`
-    $content.forEach(($_content) => $_content.classList.remove("visible"));
-    // Determina il div corretto sulla base del click
-    const id = event.target.dataset.id;
-    // Vado ad aggiungere la classe `visible` al div corretto usando l'id appena preso
-    document.getElementById(id).classList.add("visible");
-  });
-});
+// $trigger.forEach($el => {
+//   $el.addEventListener("click", (event) => {
+//     // Prima levo la classe visible da tutti i `div.content`
+//     $content.forEach(($_content) => $_content.classList.remove("visible"));
+//     // Determina il div corretto sulla base del click
+//     const id = event.target.dataset.id;
+//     // Vado ad aggiungere la classe `visible` al div corretto usando l'id appena preso
+//     document.getElementById(id).classList.add("visible");
+//   });
+// });
 
 const prev = document.querySelector('#prev')
 const next = document.querySelector('#next')
-const ultimeNovitàMenuButtons = document.querySelector('.ultimeNovità-menuButtons')
+const slider = document.querySelector('#ultimeNovità-cardMenu')
+
 prev.addEventListener('click', () => {
-  ultimeNovitàMenuButtons.scrolleft -= 300
+    slider.scrollLeft -= 100
 })
 next.addEventListener('click', () => {
-  ultimeNovitàMenuButtons.scrolleft += 300 
+  slider.scrollLeft += 100 
 })
