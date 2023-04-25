db18785b40bd5356407f45f4dbabe9c4c7f51df3
Stefano <maclion@live.com>
2023-04-25 10:28:59 +0200

    add mediaQ cardMenu e arrows

--- ultimeNovità.js
+++ ultimeNovità.js
@@ -6,11 +6,8 @@ const $content = document.querySelectorAll(".content");
 
 $trigger.forEach(($el) => {
   $el.addEventListener("click", (event) => {
-    // Prima levo la classe visible da tutti i `div.content`
     $content.forEach(($_content) => $_content.classList.remove("visible"));
-    // Determina il div corretto sulla base del click
     const id = event.target.dataset.id;
-    // Vado ad aggiungere la classe `visible` al div corretto usando l'id appena preso
     document.getElementById(id).classList.add("visible");
     console.log(id);
   });
